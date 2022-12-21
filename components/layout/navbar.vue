e1258cc64b729fa3e5eaefc2c54e22801fc868d2
corner <corner3499@gmail.com>
2022-12-21 17:57:07 +0900

    pages - app ui

--- components/layout/navbar.vue
+++ components/layout/navbar.vue
@@ -129,13 +129,6 @@
                         </svg>
                     </span>
                 </div>
-                <select class="dark:bg-black text-white"
-                    v-model="$colorMode.preference">
-                    <option value="system">System</option>
-                    <option value="light">Light</option>
-                    <option value="dark">Dark</option>
-                    <option value="sepia">Sepia</option>
-                </select>
             </div>
         </div>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,17 @@ export default defineNuxtConfig({
         viewer: true
     },
 
+    colorMode: {
+        preference: 'system', // default value of $colorMode.preference
+        fallback: 'light', // fallback value if not system preference found
+        hid: 'nuxt-color-mode-script',
+        globalName: '__NUXT_COLOR_MODE__',
+        componentName: 'ColorScheme',
+        classPrefix: '',
+        classSuffix: '-mode',
+        storageKey: 'nuxt-color-mode'
+    },
+
 
     runtimeConfig: {
         public: {
