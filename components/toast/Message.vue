20e1d7422f0b9e765fad5414e54af4b55a727f45
corner <corner3499@gmail.com>
2022-12-23 18:53:35 +0900

    temp img

--- components/toast/Message.vue
+++ components/toast/Message.vue
@@ -5,7 +5,7 @@
 
     <div id="toast-message-cta" class="p-4 w-full max-w-xs text-gray-500 bg-white rounded-lg shadow dark:bg-gray-800 dark:text-gray-400" role="alert">
         <div class="flex">
-            <img class="w-8 h-8 rounded-full shadow-lg" src="/docs/images/people/profile-picture-1.jpg" alt="Jese Leos image"/>
+            <img class="w-8 h-8 rounded-full shadow-lg" src="~/¢assets/docs/images/profile-picture-1.jpg" alt="Jese Leos image"/>
             <div class="ml-3 text-sm font-normal">
                 <span class="mb-1 text-sm font-semibold text-gray-900 dark:text-white">Jese Leos</span>
                 <div class="mb-2 text-sm font-normal">Hi Neil, thanks for sharing your thoughts regarding Flowbite.</div>
