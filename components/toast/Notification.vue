9f12436ec65579ac47bfe982c10ff7fd629134fd
corner <corner3499@gmail.com>
2022-12-23 18:56:46 +0900

    temp img

--- components/toast/Notification.vue
+++ components/toast/Notification.vue
@@ -13,7 +13,7 @@
         </div>
         <div class="flex items-center">
             <div class="inline-block relative shrink-0">
-                <img class="w-12 h-12 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Jese Leos image"/>
+                <img class="w-12 h-12 rounded-full" src="~/assets/docs/images/profile-picture-1.jpg" alt="Jese Leos image"/>
                 <span class="inline-flex absolute right-0 bottom-0 justify-center items-center w-6 h-6 bg-blue-600 rounded-full">
                 <svg aria-hidden="true" class="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                 <span class="sr-only">Message icon</span>
