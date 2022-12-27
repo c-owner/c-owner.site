df0edf73fa6a202e40058bd1c60d907af9101414
corner <corner3499@gmail.com>
2022-12-27 15:50:35 +0900

    pmtest1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -63,8 +63,8 @@ export default defineNuxtConfig({
             DATABASE_URL: process.env.DATABASE_URL
         },
         https: {
-            key: fs.readFileSync(path.join(__dirname, "cert", "/server.key"), "utf-8"),
-            cert: fs.readFileSync(path.join(__dirname, "cert", "/server_crt.pem"), "utf-8"),
+            key: process.env.NODE_ENV === 'development' ? fs.readFileSync(path.join(__dirname, "cert", "/localhost-key.pem"), "utf-8") : fs.readFileSync(path.join(__dirname, "cert", "/server.key"), "utf-8"),
+            cert: process.env.NODE_ENV === 'development' ? fs.readFileSync(path.join(__dirname, "cert", "/localhost.pem"), "utf-8") : fs.readFileSync(path.join(__dirname, "cert", "/server_crt.pem"), "utf-8"),
         }
     },
 
