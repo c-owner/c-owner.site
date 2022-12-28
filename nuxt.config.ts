b3c4bca565d1d0c600c21085c68b33ab5b8f011a
corner <corner3499@gmail.com>
2022-12-28 11:08:09 +0900

    ..

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ import path from "path";
 
 console.log(process.env.NODE_ENV === 'development');
 console.log(process.env.NODE_ENV);
+
+const ssl_key_path = process.env.SSL_KEY_PATH ? process.env.SSL_KEY_PATH : "cert/server.key";
+const ssl_cert_path = process.env.SSL_CERT_PATH ? process.env.SSL_CERT_PATH : "cert/server_crt.pem";
+
 export default defineNuxtConfig({
     // https://v3.nuxtjs.org/guide/directory-structure/nuxt.config/
     // As of RC12 Nuxt 3 supports Hybrid rendering mode
@@ -67,8 +71,8 @@ export default defineNuxtConfig({
             DATABASE_URL: process.env.DATABASE_URL ? process.env.DATABASE_URL : "mysql://admin:Rodtmxj.123@127.0.0.1:3306/corner",
         },
         https: {
-            key: process.env.NODE_ENV === 'development' ? fs.readFileSync(path.resolve(__dirname, "cert/localhost-key.pem"), "utf-8") : fs.readFileSync(path.resolve(__dirname, "cert/server.key"), "utf-8"),
-            cert: process.env.NODE_ENV === 'development' ? fs.readFileSync(path.resolve(__dirname, "cert/localhost.pem"), "utf-8") : fs.readFileSync(path.resolve(__dirname, "cert/server_crt.pem"), "utf-8")
+            key: fs.readFileSync(path.resolve(__dirname, ssl_key_path), "utf-8"),
+            cert: fs.readFileSync(path.resolve(__dirname, ssl_cert_path), "utf-8")
         }
     },
 
@@ -94,8 +98,8 @@ export default defineNuxtConfig({
                 }
             },
             https: {
-                key: process.env.NODE_ENV === 'development' ? fs.readFileSync(path.resolve(__dirname, "cert/localhost-key.pem"), "utf-8") : fs.readFileSync(path.resolve(__dirname, "cert/server.key"), "utf-8"),
-                cert: process.env.NODE_ENV === 'development' ? fs.readFileSync(path.resolve(__dirname, "cert/localhost.pem"), "utf-8") : fs.readFileSync(path.resolve(__dirname, "cert/server_crt.pem"), "utf-8")
+                key: fs.readFileSync(path.resolve(__dirname, ssl_key_path), "utf-8"),
+                cert: fs.readFileSync(path.resolve(__dirname, ssl_cert_path), "utf-8")
             }
         }
     },
